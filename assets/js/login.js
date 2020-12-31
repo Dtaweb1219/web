42651ed41cf19098f9e9b5679e64be53bfeb595d
duantangao <1257511300@qq.com>
2020-12-31 11:37:12 +0800

    获取用户的基本信息并渲染

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,19 @@
 $(function () {
-  // 点击“去注册账号”的链接
+  // 去注册
   $("#link_reg").on("click", function () {
+    // 隐藏登录框
     $(".login-box").hide();
+    // 显示注册框
     $(".reg-box").show();
   });
-
-  // 点击“去登录”的链接
   $("#link_login").on("click", function () {
-    $(".login-box").show();
+    // 隐藏注册框
     $(".reg-box").hide();
+    // 显示登录框
+    $(".login-box").show();
   });
+
+  // 自定义校验规则
   var form = layui.form;
   var layer = layui.layer;
   form.verify({
@@ -25,39 +29,43 @@ $(function () {
       }
     },
   });
+
+  // 注册功能，监听注册表单的提交事件
   $("#form_reg").on("submit", function (e) {
     e.preventDefault();
     var data = {
       username: $("#form_reg [name=username]").val(),
       password: $("#form_reg [name=password]").val(),
     };
-    $.post(
-      "http://ajax.frontend.itheima.net/api/reguser",
-      data,
-      function (res) {
-        if (res.status !== 0) {
-          // return console.log("注册失败", res.message);
-          return layer.msg(res.message);
-        }
-        // console.log("注册成功！");
-        layer.msg("注册成功，请登录！");
-        // 主动触发点击事件
-        $("#link_login").click();
+    $.post("/api/reguser", data, function (res) {
+      if (res.status !== 0) {
+        // return console.log('注册失败', res.message);
+        return layer.msg(res.message);
       }
-    );
+      // console.log('注册成功！');
+      layer.msg("注册成功，请登录！");
+      // 主动触发点击事件
+      $("#link_login").click();
+    });
   });
+
+  // 登录功能
   $("#form_login").on("submit", function (e) {
     e.preventDefault();
+    console.log($(this).serialize());
     $.ajax({
-      url: "http://ajax.frontend.itheima.net/api/login",
+      url: "/api/login",
       method: "POST",
+      // 快速获取表单中的数据
       data: $(this).serialize(),
-      sunccess: function (res) {
+      success: function (res) {
         if (res.status !== 0) {
-          return layer.msg("登录失败!");
+          return layer.msg("登录失败！");
         }
-        layer.msg("登录成功!");
+        layer.msg("登录成功");
+        // 存储
         localStorage.setItem("token", res.token);
+        // 跳转到后台主页
         location.href = "/index.html";
       },
     });
